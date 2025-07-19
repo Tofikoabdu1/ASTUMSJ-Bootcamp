a2711229d5fc19ff432e4697f6d094bee320ee0a
Tofikoabdu1 <165275773+Tofikoabdu1@users.noreply.github.com>
2025-07-19 22:40:41 +0300

    modified card

--- color-palette-picker/src/components/Card.jsx
+++ color-palette-picker/src/components/Card.jsx
@@ -1,18 +1,5 @@
 import React from "react";
-
+import "./card.css";
 export default function Card({ children }) {
-  return (
-    <div
-      style={{
-        maxWidth: "400px",
-        margin: "0 auto",
-        backgroundColor: "#fff",
-        padding: "20px",
-        borderRadius: "10px",
-        boxShadow: "0 4px 12px rgba(0,0,0,0.1)",
-      }}
-    >
-      {children}
-    </div>
-  );
+  return <div className="card">{children}</div>;
 }
